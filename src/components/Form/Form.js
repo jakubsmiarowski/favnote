3f31f21b228e92592d67442a46caafb9a58ebeb2
jakubsmiarowski <48868566+jakubsmiarowski@users.noreply.github.com>
2020-03-23 18:28:16 +0100

    form auth added

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,7 +4,7 @@ import styles from "./Form.module.scss";
 const Form = ({ submitFn }) => (
   <div className={styles.wrapper}>
     <h2>Add new twitter account</h2>
-    <form className={styles.form} onSubmit={submitFn}>
+    <form autoComplete="off" className={styles.form} onSubmit={submitFn}>
       <div className={styles.formItem}>
         <input
           className={styles.input}
@@ -13,28 +13,54 @@ const Form = ({ submitFn }) => (
           id="name"
           placeholder=" "
           maxLength="30"
+          required
         />
-        <label className={styles.label} htmlFor="name">Name</label>
+        <label className={styles.label} htmlFor="name">
+          Name
+        </label>
         <div className={styles.formItemBar} />
       </div>
       <div className={styles.formItem}>
-        <input className={styles.input} type="text" name="link" id="link" placeholder=" " />
-        <label className={styles.label} htmlFor="link">Link</label>
+        <input
+          className={styles.input}
+          type="text"
+          name="link"
+          id="link"
+          placeholder=" "
+          required
+        />
+        <label className={styles.label} htmlFor="link">
+          Link
+        </label>
         <div className={styles.formItemBar} />
       </div>
       <div className={styles.formItem}>
-        <input className={styles.input} type="text" name="image" id="image" placeholder=" " />
-        <label className={styles.label} htmlFor="image">Image</label>
+        <input
+          className={styles.input}
+          type="text"
+          name="image"
+          id="image"
+          placeholder=" "
+        />
+        <label className={styles.label} htmlFor="image">
+          Image
+        </label>
         <div className={styles.formItemBar} />
       </div>
       <div className={styles.formItem}>
-        <textarea className={styles.textarea} name="description" id="description" placeholder=" " />
-        <label className={styles.label} htmlFor="description">Description</label>
+        <textarea
+          className={styles.textarea}
+          name="description"
+          id="description"
+          placeholder=" "
+          required
+        />
+        <label className={styles.label} htmlFor="description">
+          Description
+        </label>
         <div className={styles.formItemBar} />
       </div>
-      <button className={styles.button}>
-        add new item
-      </button>
+      <button className={styles.button}>add new item</button>
     </form>
   </div>
 );
