9fcd613e2a3c06cee6d927ca05ac0a56a615ed28
jakubsmiarowski <48868566+jakubsmiarowski@users.noreply.github.com>
2020-03-23 16:38:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ListWrapper from './components/ListWrapper/ListWrapper';
+import Form from './components/Form/Form';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialStateItems = [
+    {
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Malevich.black-square.jpg/605px-Malevich.black-square.jpg',
+        name: 'Dan Abramov',
+        description: 'Working on @reactjs. The demo guy.',
+        twitterLink: 'https://twitter.com/dan_abramov',
+    },
+    {
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Malevich.black-square.jpg/605px-Malevich.black-square.jpg',
+        name: 'Ryan Florence',
+        description: 'Making React accessible for users and developers at https://reach.tech . Online learning, workshops, OSS, and consulting.',
+        twitterLink: 'https://twitter.com/ryanflorence',
+    },
+    {
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Malevich.black-square.jpg/605px-Malevich.black-square.jpg',
+        name: 'Michael Jackson',
+        description: 'Maker. Co-author of React Router. Working on @ReactTraining. Created @unpkg. Head over heels for @cari.',
+        twitterLink: 'https://twitter.com/mjackson',
+    },
+    {
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Malevich.black-square.jpg/605px-Malevich.black-square.jpg',
+        name: 'Kent C. Dodds',
+        description: 'Making software development more accessible · Husband, Father, Latter-day Saint, Teacher, OSS, GDE, @TC39 · @PayPalEng @eggheadio @FrontendMasters · #JS',
+        twitterLink: 'https://twitter.com/kentcdodds',
+    },
+];
+
+class App extends React.Component {
+    state = {
+        items:[...initialStateItems],
+    }
+
+    addItem = (e) => {
+        e.preventDefault();
+
+        const newItem = {
+            name: e.target[0].value,
+            twitterLink: e.target[1].value,
+            image: e.target[2].value,
+            description: e.target[3].value,
+        }
+
+        this.setState(prevState => ({
+            items: [...prevState.items, newItem],
+        }));
+
+        e.target.reset();
+    }
+
+    render() {
+        return (
+            <div>
+                <ListWrapper 
+                    items={this.state.items}
+                />
+                <Form submitFn={this.addItem} />
+            </div>
+        )
+    }
+        
 }
 
 export default App;
